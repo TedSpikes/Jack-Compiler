7728f11269e6b3126f6eb1f1df6762651facdd3b
Ted Spikes <ted@tedspikes.com>
2020-01-21 20:28:45 +0200

    Initial Commit

---
+++ Jack Compiler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jack Compiler
+//
+//  Created by Fedor on 1/21/20.
+//  Copyright © 2020 Ted Kostylev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
